6cca661840eac08e59049cce3917caa3589589c1
wafula <wafulaat@gmail.com>
2026-05-17 21:12:46 +0300

    refactor(permissions): shared county filter for scholar doctypes

--- education/permissions/scholar_transfer.py
+++ education/permissions/scholar_transfer.py
@@ -1,33 +1,11 @@
-import frappe
+from education.permissions.utils import get_user_county_filter
 
 
-def get_account_query_conditions(user=None):
-	if not user:
-		user = frappe.session.user
+def get_county_query_conditions(user=None):
+    allowed_counties = get_user_county_filter(user)
 
-	if "System Manager" in frappe.get_roles(user):
-		return ""
-
-	regions = frappe.get_all(
-		"NL Region", filters=[["user_group", "!=", ""]], pluck="user_group"
-	)
-	if not regions:
-		return ""
-
-	allowed_regions = frappe.get_all(
-		"User Group Member",
-		filters=[["parent", "in", regions], ["user", "=", user]],
-		pluck="parent",
-	)
-
-	allowed_counties = frappe.get_all(
-		"Region County", filters=[["parent", "in", allowed_regions]], pluck="county"
-	)
-
-	escaped = ",".join([frappe.db.escape(val) for val in allowed_counties])
-
-	return f"""
+    return f"""
         (
-            `tabScholar Transfer`.county_of_residence IN ({escaped})
+            `tabScholar Transfer`.county_of_residence IN ({allowed_counties})
         )
         """
